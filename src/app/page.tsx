e9ffe73882bab335de9624a01399ee559cf376bf
jun <emfowkd@gmail.com>
2025-02-19 16:20:08 +0900

    1-3 User, Admin 페이지 생성하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+    <main >
+      누구나 볼 수 있는 페이지입니다.
+    </main>
   );
 }
