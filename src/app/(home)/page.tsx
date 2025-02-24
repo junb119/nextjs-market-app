0c37d8d365ff525ade14a1357ae253f3b88b483e
jun <emfowkd@gmail.com>
2025-02-24 18:15:29 +0900

    3-2 홈 페이지 UI 생성하기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,36 @@
+import Container from "@/components/Container";
 import getProducts, { productParams } from "../actions/getProducts";
+import EmptyState from "@/components/EmptyState";
+import ProductCard from "@/components/ProductCard";
+import getCurrentUser from "../actions/getCurrentUser";
+import FloatingButton from "@/components/FloatingButton";
 
 interface HomeProps {
   searchParams: productParams;
 }
 export default async function Home({ searchParams }: HomeProps) {
   const products = await getProducts(searchParams);
+  const currentUser = await getCurrentUser();
   console.log("@@products", products);
-  return <main>누구나 볼 수 있는 페이지입니다.</main>;
+  return (
+    <Container>
+      {/* Category */}
+      {products?.data.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <>
+          <div className="grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+            {products.data.map((product) => (
+              <ProductCard
+                key={product.id}
+                currentUser={currentUser}
+                data={product}
+              />
+            ))}
+          </div>
+        </>
+      )}
+      <FloatingButton href="/products/upload">+</FloatingButton>
+    </Container>
+  );
 }
