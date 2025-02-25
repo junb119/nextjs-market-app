28c0dad24454210855b526b17e7ecb5e9c62d78c
jun <emfowkd@gmail.com>
2025-02-25 16:14:14 +0900

    3-5 Favorite Route

---
+++ src/app/api/favorites/[productId]/route.ts
@@ -0,0 +1,28 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+interface Params {
+  productId?: string;
+}
+export async function POST(request: Request, { params }: { parmas: Params }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+  const { productId } = params;
+  if (!productId || typeof productId !== "string") {
+    throw new Error("Invalid ID");
+  }
+  const favoritedIds = [...(currentUser.favoriteIds || [])];
+  favoritedIds.push(productId);
+
+  const user = await prisma?.user.update({
+    where: { id: currentUser.id },
+    data: {
+      favoriteIds: favoritedIds,
+    },
+  });
+  return NextResponse.json(user);
+}
+
+export async function DELETE();
