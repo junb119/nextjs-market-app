f4abd94ab6033926066bd5affc4191869212ff4c
jun <emfowkd@gmail.com>
2025-02-25 16:38:49 +0900

    3-5-Favorite-Route-2

--- src/app/api/favorites/[productId]/route.ts
+++ src/app/api/favorites/[productId]/route.ts
@@ -25,4 +25,24 @@ export async function POST(request: Request, { params }: { parmas: Params }) {
   return NextResponse.json(user);
 }
 
-export async function DELETE();
+export async function DELETE(request: Request, { params }: { parmas: Params }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+  const { productId } = params;
+  if (!productId || typeof productId !== "string") {
+    throw new Error("Invalid ID");
+  }
+  let favoritedIds = [...(currentUser.favoriteIds || [])];
+  favoritedIds = favoritedIds.filter(id => id !== productId);
+
+  const user = await prisma?.user.update({
+    where: { id: currentUser.id },
+    data: {
+      favoriteIds: favoritedIds,
+    },
+  });
+  return NextResponse.json(user);
+}
+
