3ef5a15bdba9c0f799c71dd9eec5cfe21309d93a
jun <emfowkd@gmail.com>
2025-02-25 17:49:52 +0900

    3-8 Categories 컴포넌트 생성하기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ import EmptyState from "@/components/EmptyState";
 import ProductCard from "@/components/ProductCard";
 import getCurrentUser from "../actions/getCurrentUser";
 import FloatingButton from "@/components/FloatingButton";
+import Categories from "@/components/categories/Categories";
 
 interface HomeProps {
   searchParams: productParams;
@@ -14,9 +15,9 @@ export default async function Home({ searchParams }: HomeProps) {
   console.log("@@products", products);
   return (
     <Container>
-      {/* Category */}
+      <Categories />
       {products?.data.length === 0 ? (
-        <EmptyState />
+        <EmptyState showReset />
       ) : (
         <>
           <div className="grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
