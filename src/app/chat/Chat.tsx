cb28867760c594d14dc3f9b859def2780239f188
jun <emfowkd@gmail.com>
2025-02-26 16:16:23 +0900

    5-12 ChatHeader 컴포넌트 생성하기

--- src/app/chat/Chat.tsx
+++ src/app/chat/Chat.tsx
@@ -1,6 +1,8 @@
-import { TUserWithChat } from "@/types";
+import { TConversation, TUserWithChat } from "@/types";
 import React from "react";
 import Input from "./Input";
+import { User } from "@prisma/client";
+import ChatHeader from "./ChatHeader";
 
 interface ChatProps {
   receiver: {
@@ -12,11 +14,26 @@ interface ChatProps {
   setLayout: (layout: boolean) => void;
 }
 const Chat = ({ currentUser, receiver, setLayout }: ChatProps) => {
+  const conversation: TConversation | undefined =
+    currentUser?.conversations.find((conversation: TConversation) =>
+      conversation.users.find((user: User) => user.id === receiver.receiverId)
+    );
   if (!receiver.receiverName || !currentUser)
     return <div className="w-full h-full"></div>;
   return (
     <div className="w-full">
-      <div>{/* Chat Header */}</div>
+      <div>
+        <ChatHeader
+          setLayout={setLayout}
+          receiverName={receiver.receiverName}
+          receiverImage={receiver.receiverImage}
+          lastMessageTime={
+            conversation?.messages
+              .filter((message) => message.receiverId === currentUser.id)
+              .slice(-1)[0]?.createdAt
+          }
+        />
+      </div>
       <div className="flex flex-col gap-8 p-4 overflow-auto h-[calc(100vh_-_60px-70px_-_80px)]">
         {/* Chat Message */}
       </div>
--- src/app/chat/ChatHeader.tsx
+++ src/app/chat/ChatHeader.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
-const ChatHeader = () => {
-  return (
-    <div>ChatHeader</div>
-  )
+import Avatar from "@/components/Avatar";
+import { formatTime } from "@/helpers/dayjs";
+import React from "react";
+import { IoChevronBackCircleSharp } from "react-icons/io5";
+interface ChatHeaderProps {
+  setLayout: (layout: boolean) => void;
+  receiverName: string;
+  receiverImage: string;
+  lastMessageTime: Date | undefined;
 }
 
-export default ChatHeader
+const ChatHeader = ({
+  setLayout,
+  receiverName,
+  receiverImage,
+  lastMessageTime,
+}: ChatHeaderProps) => {
+  return (
+    <div className="pl-4 border-b-[1px]">
+      <div className="flex items-center h-16 gap-4">
+        <div className="flex items-center justify-center text-3xl text-gray-400 hover:text-gray-600">
+          <button onClick={() => setLayout(false)} className="md:hidden">
+            <IoChevronBackCircleSharp />
+          </button>
+        </div>
+        <div className="flex items-center gap-[0.6rem]">
+          <div className="flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-900 rounded-full">
+            <Avatar src={receiverImage} />
+          </div>
+          <div>
+            <h2 className="text-lg font-semibold">{receiverName}</h2>
+            {lastMessageTime && (
+              <p className="text-gray-600">{formatTime(lastMessageTime)}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
