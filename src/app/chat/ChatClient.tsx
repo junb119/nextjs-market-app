4251ad996f0823f3e05e19ac66e060dcce4da3bc
jun <emfowkd@gmail.com>
2025-02-26 14:15:41 +0900

    5-7 SWR

--- src/app/chat/ChatClient.tsx
+++ src/app/chat/ChatClient.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { TUserWithChat } from "@/types";
 import { User } from "@prisma/client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 interface ChatClientProps {
   currentUser?: User | null;
 }
@@ -12,10 +14,23 @@ const ChatClient = ({ currentUser }: ChatClientProps) => {
     receiverImage: "",
   });
   const [layout, setLayout] = useState(false);
-  useEffect(() => {
-    axios.get(`/api/chat`).then((res) => console.log(res));
-  }, []);
 
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useSWR("/api/chat", fetcher, { refreshInterval: 1000 });
+
+  console.log(users);
+  // useEffect(() => {
+  //   axios.get(`/api/chat`).then((res) => console.log(res));
+  // }, []);
+
+  users?.find((user: TUserWithChat) => user.email === currentUser?.email);
+
+  if (isLoading) return <p>Loading...!</p>;
+  if (error) return <p>Error!</p>;
   return (
     <main>
       <div className="grid grid-cols-[1fr] md:grid-cols-[300px_1fr]">
